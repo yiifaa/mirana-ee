48ab485c203f8edb96f1022c1881afdeaad417de
Huan Gan <Huan Gan>
2017-08-25 15:40:37 +0800

    update the project

---
+++ src/main/java/com/mirana/domain/Account.java
@@ -0,0 +1,44 @@
+package com.mirana.domain;
+
+/**
+ * 
+ * 
+ * @author  <a href="mailto:ganhuanxp@163.com">甘焕</a>
+ * @version  1.0
+ * 开发日期：2017年8月25日 ： 上午10:33:50 
+ */
+public class Account {
+	
+	private String username;
+	
+	private String password;
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
---
+++ src/main/java/com/mirana/domain/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * 
+ * @author  <a href="mailto:ganhuanxp@163.com">甘焕</a>
+ * @version  1.0
+ * 开发日期：2017年8月25日 ： 上午10:33:36 
+ */
+package com.mirana.domain;
